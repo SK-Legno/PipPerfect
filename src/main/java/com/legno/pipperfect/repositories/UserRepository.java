7c3ac50e2ca7d0b78f72a04a8062b048ea34a658
legno <legno.leon@gmail.com>
2024-10-02 10:25:01 +0900

    ホーム画面表示v0.1

---
+++ src/main/java/com/legno/pipperfect/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.legno.pipperfect.repositories;
+
+import com.legno.pipperfect.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
