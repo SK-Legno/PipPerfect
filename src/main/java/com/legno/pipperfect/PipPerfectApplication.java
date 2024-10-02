6a96261955c94cd5ee1863b5281609cb834964fe
legno <legno.leon@gmail.com>
2024-10-02 09:03:00 +0900

    Initial commit

---
+++ src/main/java/com/legno/pipperfect/PipPerfectApplication.java
@@ -0,0 +1,13 @@
+package com.legno.pipperfect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipPerfectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PipPerfectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/legno/pipperfect/PipPerfectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.legno.pipperfect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipPerfectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
